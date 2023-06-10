12bd00e7bc1d08d08a71235b1d0048bb2e728481
Kevin Garner <kevinleegarner@gmail.com>
2023-06-09 23:43:15 -0500

    Made svelte work with Foundry and provided a sample store working across two components.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
-import './app.css'
-import App from './App.svelte'
+import App from "./App.svelte";
 
-const app = new App({
-  target: document.getElementById('app'),
-})
+declare global {
+  var Hooks: {
+    on: any;
+  };
+  var Application: any;
+  var foundry: any;
+}
 
-export default app
+class SveltInFoundry extends Application {
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      height: "auto",
+      id: "todo-list",
+      template: `modules/svelte-in-foundry/templates/shell.hbs`,
+      popOut: true,
+    });
+  }
+
+  activateListeners(html) {
+    super.activateListeners(html);
+    console.log("Svelte in Foundry | Activate Listeners");
+    this.component = new App({
+      target: html.get(0),
+      props: {
+        name: "Foundry",
+      },
+    });
+  }
+}
+
+Hooks.on("ready", () => {
+  const app1 = new SveltInFoundry();
+  app1.render(true);
+
+  const app2 = new SveltInFoundry();
+  app2.render(true);
+});
--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         globals: {
-          svelte: "Svelte",
+          svelte: "svelte",
         },
       },
     },
